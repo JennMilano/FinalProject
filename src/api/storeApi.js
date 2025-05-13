d87b5c382aff3dbd7976da7db7cea0a1206d069b
Jenn Milano <jmilano0354@gmail.com>
2025-05-12 21:00:39 -0400

    updated environment variables

--- src/api/storeApi.js
+++ src/api/storeApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const storeApi = createApi({
     reducerPath: "storeApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
     prepareHeaders: (headers, {getState}) => {
         const token = getState().auth.token;
         if (token) {
